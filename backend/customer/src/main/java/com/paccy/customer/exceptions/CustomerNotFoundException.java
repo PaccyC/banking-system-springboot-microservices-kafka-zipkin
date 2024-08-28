ebfa060c83361c1b2836e20c83b14e8acf252a53
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-28 10:45:18 +0200

    feat:exception handling

--- backend/customer/src/main/java/com/paccy/customer/exceptions/CustomerNotFoundException.java
+++ backend/customer/src/main/java/com/paccy/customer/exceptions/CustomerNotFoundException.java
@@ -1,5 +1,6 @@
 package com.paccy.customer.exceptions;
 
+//Exception handler
 public class CustomerNotFoundException  extends     RuntimeException{
     public CustomerNotFoundException(String message) {
         super(message);
