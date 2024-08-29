994e2fd1713a6605aa49cfc580ab654df5dd4516
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-29 14:14:19 +0200

    feat: initialized AccountService,AccountController and AccountRepository

--- backend/customer-account/src/main/java/com/paccy/customer_account/controllers/AccountController.java
+++ backend/customer-account/src/main/java/com/paccy/customer_account/controllers/AccountController.java
@@ -1,4 +1,14 @@
 package com.paccy.customer_account.controllers;
 
+import com.paccy.customer_account.services.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/account")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
 }
--- backend/customer-account/src/main/java/com/paccy/customer_account/services/AccountService.java
+++ backend/customer-account/src/main/java/com/paccy/customer_account/services/AccountService.java
@@ -1,4 +1,12 @@
 package com.paccy.customer_account.services;
 
+import com.paccy.customer_account.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
 }
