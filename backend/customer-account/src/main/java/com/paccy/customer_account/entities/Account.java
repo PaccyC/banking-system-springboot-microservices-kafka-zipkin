37953d7d6f26681dad31395873e6013ae60e372a
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-29 14:11:20 +0200

    feat: Created Account Controller

--- backend/customer-account/src/main/java/com/paccy/customer_account/entities/Account.java
+++ backend/customer-account/src/main/java/com/paccy/customer_account/entities/Account.java
@@ -1,4 +1,41 @@
 package com.paccy.customer_account.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE
+    )
+    private Integer id;
+
+    private Integer customerId;
+
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+
+    private String balance;
+
+    @Column( updatable = false)
+    @CreatedDate
+    private LocalDate createDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDate updateDate;
+
 }
