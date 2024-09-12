73499cbb94586b4346f4763bdde1f912ea2dbf44
PaccyC <abayisengaaimepacifique@gmail.com>
2024-09-12 20:07:01 +0200

    feat: modified UserDetailServiceImpl

--- backend/customer/src/main/java/com/paccy/customer/auth/services/UserDetailsServiceImpl.java
+++ backend/customer/src/main/java/com/paccy/customer/auth/services/UserDetailsServiceImpl.java
@@ -1,11 +1,16 @@
 package com.paccy.customer.auth.services;
 
+import com.paccy.customer.entities.Customer;
 import com.paccy.customer.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +19,15 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return customerRepository.findByEmail(email)
+        Customer customer= customerRepository.findByEmail(email)
                 .orElseThrow(()-> new UsernameNotFoundException("Customer not found"));
+        if (customer == null){
+            throw new UsernameNotFoundException("Customer not found");
+        }
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        return new org.springframework.security.core.userdetails.User(
+                customer.getEmail(),
+                customer.getPassword(),
+                authorities);
     }
 }
