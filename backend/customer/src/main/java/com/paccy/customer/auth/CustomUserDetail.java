c6ba5cd847414cb0ae25092b81bea065450df3b7
PaccyC <abayisengaaimepacifique@gmail.com>
2024-09-12 19:55:13 +0200

    feat: Added CustomUserDetail clas

---
+++ backend/customer/src/main/java/com/paccy/customer/auth/CustomUserDetail.java
@@ -0,0 +1,51 @@
+package com.paccy.customer.auth;
+
+import com.paccy.customer.entities.Customer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private Customer customer;
+    public CustomUserDetail(Customer customer) {
+        this.customer = customer;
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return customer.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
