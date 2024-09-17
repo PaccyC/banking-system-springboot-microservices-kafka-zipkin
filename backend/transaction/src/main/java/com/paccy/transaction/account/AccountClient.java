f9a86ec23508d41cdf4ec9c9327b1cdb1bff969c
PaccyC <abayisengaaimepacifique@gmail.com>
2024-09-17 23:37:18 +0200

    set up controllers, services and repository in transaction ms

---
+++ backend/transaction/src/main/java/com/paccy/transaction/account/AccountClient.java
@@ -0,0 +1,8 @@
+package com.paccy.transaction.account;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+
+@FeignClient
+public class AccountClient {
+}
