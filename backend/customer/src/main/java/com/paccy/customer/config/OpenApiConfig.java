c35e18ef85e0b85fd0cb39b9c1cc9f649846ddf2
PaccyC <abayisengaaimepacifique@gmail.com>
2024-09-18 22:17:29 +0200

    customer-service: api documentation in json format

--- backend/customer/src/main/java/com/paccy/customer/config/OpenApiConfig.java
+++ backend/customer/src/main/java/com/paccy/customer/config/OpenApiConfig.java
@@ -6,28 +6,28 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import io.swagger.v3.oas.models.OpenAPI;
-//
-//@Configuration
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
 public class OpenApiConfig {
-//
-//    @Bean
-//    public OpenAPI customerServiceOpenApi(){
-//
-//    return new OpenAPI()
-//            .info( new Info()
-//                    .title("Customer Service APIs")
-//                    .description("Customer Service APIs documentation")
-//                    .version("v1.0")
-//                    .contact(new Contact().email("paccy7002@gmail.com").url("https://github.com/PaccyC").name("Paccy"))
-//                    .license(new License().name("API License").url("API license URL"))
-//
-//
-//            )
-//            .externalDocs(new ExternalDocumentation().description("Customer Service APIs")
-//                    .url("www.example.com"));
-//
-//    }
+
+    @Bean
+    public OpenAPI customerServiceOpenApi(){
+
+    return new OpenAPI()
+            .info( new Info()
+                    .title("Customer Service APIs")
+                    .description("Customer Service APIs documentation")
+                    .version("v1.0")
+                    .contact(new Contact().email("paccy7002@gmail.com").url("https://github.com/PaccyC").name("Paccy"))
+                    .license(new License().name("Apache 2.0").url("API license URL"))
+
+
+            )
+            .externalDocs(new ExternalDocumentation().description("Customer Service APIs")
+                    .url("www.example.com"));
+
+    }
 
 
 }
