7eb99b586235075b0a8d4a9b1cdd650dfd87fae3
PaccyC <abayisengaaimepacifique@gmail.com>
2024-11-09 21:36:08 +0200

    [page]:finished Balances page

---
+++ frontend/src/components/BalanceComponent.tsx
@@ -0,0 +1,53 @@
+
+import { FiChevronRight } from "react-icons/fi";
+
+interface BalanceComponentProps{
+    balanceType: string;
+    accountNumber: string;
+    totalAmount: number;
+    bankName: string;
+    bankImage?: string;
+}
+const BalanceComponent = ({balanceType,accountNumber,totalAmount,bankName,bankImage}:BalanceComponentProps) => {
+  return (
+    <div className=" bg-white min-h-[200px] rounded-[8px] p-6 flex flex-col gap-4">
+        <div className=" flex justify-between items-center">
+            <h3 className="text-[16px] font-inter font-bold text-gray-1">{balanceType}</h3>
+            <div className=" flex items-center gap-3">
+                <p className=" text-[#666666] font-inter font-medium text-[14px]">{bankName}</p>
+                {bankImage && 
+                 <img src={bankImage}  />
+                }
+                
+            </div>
+        </div>
+
+        <hr className=" h-[2px] bg-light-gray-2"/>
+        <div className=" flex flex-col gap-3">
+           <div className=" flex flex-col gap-2">
+            <h3 className=" font-inter font-semibold text-[20px] text-dark-1">{accountNumber}</h3>
+            <p className=" font-inter font-normal tet-[14px] text-light-gray-4">Account Number</p>
+           </div>
+           <div className=" flex flex-col gap-2">
+            <h3 className=" font-inter font-semibold text-[20px] text-dark-1">${totalAmount}</h3>
+            <p className=" font-inter font-normal tet-[14px] text-light-gray-4">Total amount</p>
+           </div>
+        </div>
+
+        {/* Remove and details */}
+
+        <div className=" flex justify-between items-center">
+            <p 
+            onClick={()=>{}}
+            className="font-inter font-normal tet-[14px] text-primary-1"
+            >Remove</p>
+
+            <button className=" text-white bg-primary-1 rounded-[4px] py-2 px-5 font-inter font-medium text-[14px] flex gap-2 items-center">
+                Details <FiChevronRight className=" text-white"/>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default BalanceComponent
--- frontend/src/pages/Balances.tsx
+++ frontend/src/pages/Balances.tsx
@@ -1,7 +1,58 @@
+import BalanceComponent from "../components/BalanceComponent"
 
 const Balances = () => {
   return (
-    <div>Balances</div>
+    <div className=" w-full flex flex-col gap-4">
+      <h3 className=" font-inter font-normal text-[22px] text-gray-1">Balances</h3>
+
+      <div className=" grid grid-cols-3 gap-4 flex-grow">
+        <BalanceComponent
+         balanceType="Credit Card"
+          accountNumber="3388 4556  8860 8***"
+          totalAmount={25000}
+          bankName="Master Card"
+          bankImage="/icons/Mastercard.svg"
+        />
+           <BalanceComponent
+         balanceType="Credit Card"
+          accountNumber="3388 4556  8860 8***"
+          totalAmount={25000}
+          bankName="Master Card"
+          bankImage="/icons/Mastercard.svg"
+        />
+           <BalanceComponent
+         balanceType="Credit Card"
+          accountNumber="3388 4556  8860 8***"
+          totalAmount={25000}
+          bankName="Master Card"
+          bankImage="/icons/Mastercard.svg"
+        />
+           <BalanceComponent
+         balanceType="Credit Card"
+          accountNumber="3388 4556  8860 8***"
+          totalAmount={25000}
+          bankName="Master Card"
+          bankImage="/icons/Mastercard.svg"
+        />
+           <BalanceComponent
+         balanceType="Credit Card"
+          accountNumber="3388 4556  8860 8***"
+          totalAmount={25000}
+          bankName="Master Card"
+          bankImage="/icons/Mastercard.svg"
+        />
+
+        <div className=" bg-white min-h-[200px] h-full rounded-[8px] px-24 flex flex-col gap-4 items-center justify-center">
+        <button className=" text-white w-full bg-primary-1 rounded-[4px] py-2 px-5 font-inter font-bold text-[16px] flex gap-2 items-center justify-center">
+                Add Accounts 
+        </button>
+        <p 
+        onClick={()=>{}}
+        className=" text-gray-1 font-inter font-medium text-[16px] cursor-pointer ">Edit Accounts </p>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
