9ff3188f816ddddafb6f4e27e6babc3c003fbac8
PaccyC <abayisengaaimepacifique@gmail.com>
2024-11-12 21:58:07 +0200

    comments in fileuploader

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -1,71 +1,12 @@
-import  {useCallback, useEffect, useState} from 'react'
-// import {FileWithPath, useDropzone} from 'react-dropzone'
-import { Button } from '../components/ui/button';
 
+const FileUploader = () => {
 
-interface Props{
-    fieldChange: (Files : File[])=>void;
-    mediaUrl: string;
-}
-const FileUploader = ({fieldChange}:Props) => {
-
-    const [files, setFiles] = useState<File[]>([]);
-    const [fileUrl,setFileUrl]= useState("");
-
-    
-  //   useEffect(() => {
-  //     if (mediaUrl) {
-  //         setFileUrl(mediaUrl);
-  //     }
-  // }, [mediaUrl]);
   
-    // const onDrop = useCallback(
-    //   (acceptedFiles:FileWithPath[] )=>{
-    //     setFiles(acceptedFiles)
-    //     fieldChange(acceptedFiles);
-    //     setFileUrl(URL.createObjectURL(acceptedFiles[0]))
-        
-    // },[files])
-    // 
-    // const {getRootProps, getInputProps} = useDropzone({
-    //     onDrop,
-    // accept:{
-    //     "image/*":[".png",".jpg",".gif",".svg"]
-    // }})
+    
+
   return (
-    <div
-    //  {...getRootProps()}
-         className=' flex flex-col flex-center bg-dark-3 rounded-xl cursor-pointer' >
-      <input 
-      // {...getInputProps()}  
-      className=' cursor-pointer'/>
-      {
-        fileUrl ? 
-        <>
-       
-        <div className=' flex flex-1 justify-center w-full p-5 lg:p-10'>
-            <img 
-              src={fileUrl} 
-              alt="uploader-image"
-              className='h-80 lg:h-[480px] w-full rounded-[24px] object-cover object-top '
-              />
-        </div>
-        <p className='text-light-4 text-center small-regular w-full p-4 border-t border-t-dark-4'>Click or drag a photos to replace</p>
-        </>
-             : 
-        <div className=' flex-col flex-center h-80 p-7 lg:h-[621px]'>
-         <img 
-            src="/assets/icons/file-upload.svg" 
-            alt="upload" 
-         />
-         <h3 className=' base-medium text-light-2 mb-2 mt-6'>Drag photo here</h3>
-         <p className=' text-light-4 small-regular mb-6 '>SVG,PNG,JPG</p>
-         <Button className=' h-12 bg-dark-4 px-5 text-light-1 flex gap-2'>
-            Upload from computer
-         </Button>
-        </div>
-        
-      }
+    <div>
+
     </div>
   )
 }
--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -14,13 +14,13 @@ import { z } from "zod"
 import { toast } from "../hooks/use-toast"
 import {
   Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  // FormControl,
+  // FormField,
+  // FormItem,
+  // FormLabel,
+  // FormMessage,
 } from "../components/ui/form"
-import FileUploader from "../components/FileUploader"
+// import FileUploader from "../components/FileUploader"
 
 
 
@@ -128,7 +128,7 @@ const Settings = () => {
 
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-      <FormField
+      {/* <FormField
         control={form.control}
         name="username"
         render={({ field }) => (
@@ -143,7 +143,7 @@ const Settings = () => {
             <FormMessage className=" text-red" />
           </FormItem>
         )}
-      />
+      /> */}
         <Button type="submit">Submit</Button>
       </form>
     </Form>
