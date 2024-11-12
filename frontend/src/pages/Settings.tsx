e0bf17b1cc008cec2aaf92cf14e8ed56847c0864
PaccyC <abayisengaaimepacifique@gmail.com>
2024-11-12 16:22:29 +0200

    added toast

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -6,6 +6,45 @@ import {
   TabsTrigger,
 } from "../components/ui/tabs"
 import { Button } from "../components/ui/button"
+// import FileUploader from "../components/FileUploader"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { toast } from "../hooks/use-toast"
+import {
+  Form,
+  // FormControl,
+  // FormField,
+  // FormItem,
+  // FormLabel,
+  // FormMessage,
+} from "../components/ui/form"
+
+const FormSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
+  
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+      defaultValues: {
+        username: "",
+      },
+    })
+  
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+      toast({
+        title: "You submitted the following values:",
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      })
+    }
 
 const Settings = () => {
   const [selectedTab, setSelectedTab] = useState("account")
@@ -82,7 +121,36 @@ const Settings = () => {
       </Tabs>
       {
         selectedTab === "account" && 
-        <div></div>
+        <div className=" p-24">
+     
+
+
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+      {/* <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className=" text-white">Add Photos</FormLabel>
+            <FormControl>
+              <FileUploader
+                fieldChange= {field.onChange}
+                mediaUrl={post?.imageUrl || ""}
+             
+              />
+            </FormControl>
+          
+            <FormMessage className=" text-red" />
+          </FormItem>
+        )}
+      /> */}
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+
+
+        </div>
       }
     </div>
   )
